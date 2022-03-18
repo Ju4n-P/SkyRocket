6ea1edb19a585e23015ff70829c26f7855df38de
Ju4n-P <th3dr34mm4k3r@gmail.com>
2022-03-18 19:13:15 +0100

    try to put all by default

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const corsOptions = {
   'methods': 'GET,HEAD,PUT,PATCH,POST,DELETE',
   'preflightContinue': false
 }
-app.use(cors());
+app.use(cors(corsOptions));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
